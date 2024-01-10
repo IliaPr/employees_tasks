b23a5f5e25df76fec211d8da841808c781a73446
IliaPr <ilya.prianichnikov@gmail.com>
2024-01-10 20:34:20 +0700

    models changes

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
 # models.py
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, Text, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship, declarative_base
-from pydantic import BaseModel
-from datetime import datetime
+
 
 Base = declarative_base()
 
@@ -16,7 +15,7 @@ class Task(Base):
     executor_id = Column(Integer, ForeignKey('employees.id'))
     deadline = Column(DateTime)
     status = Column(String)
-    description = Column(String)  # Добавьте эту строку для описания задачи
+    description = Column(String)
     executor = relationship("Employee", back_populates="tasks")
     subtasks = relationship("Task", back_populates="parent_task", remote_side=[id])
     parent_task = relationship("Task", back_populates="subtasks", foreign_keys=[parent_task_id])
@@ -28,5 +27,5 @@ class Employee(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, index=True)
     position = Column(String)
-    is_busy = Column(Boolean, default=False)  # Добавьте эту строку, если она отсутствует
+    is_busy = Column(Boolean, default=False)
     tasks = relationship("Task", back_populates="executor")
